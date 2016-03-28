d4c6ee3bd1a3325c1c8ed595b265156c83a5fc34
David A. DaSilva Jr <david_dasilva@apple.com>
2016-03-28 19:24:05 -0400

    Initial Commit

---
+++ tree traversal/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tree traversal
+//
+//  Created by David Anthony DaSilva on 3/28/16.
+//  Copyright © 2016 David Anthony DaSilva. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
